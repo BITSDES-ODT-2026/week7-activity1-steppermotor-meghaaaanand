2caceec3c50ca4fc25dfa3ec52fb0141a49a1786
Megha <megha_anand@me.com>
2026-02-16 15:06:14 +0530

    Update StepperMotor-Pro.py

--- StepperMotor-Pro.py
+++ StepperMotor-Pro.py
@@ -4,35 +4,20 @@ mag1 = Pin(12, Pin.OUT)
 mag2 = Pin(25, Pin.OUT)
 mag3 = Pin(4, Pin.OUT)
 mag4 = Pin(18, Pin.OUT)
+list1 = [[1,0,0,0],[0,1,0,0],[0,0,1,0],[0,0,0,1]]
+list2 = [[0,0,0,1],[0,0,1,0],[0,1,0,0],[1,0,0,0]]
 while True:
+    for i in range(1,500):
+        for k in list1:
+            mag1.value(k[0])
+            mag2.value(k[1])
+            mag3.value(k[2])
+            mag4.value(k[3])
+            time.sleep(0.003)
     for i in range (1,500):
-        mag1.value(0)
-        mag2.value(0)
-        mag3.value(0)
-        mag4.value(1)
-        time.sleep(0.005)
-        
-        mag1.value(0)
-        mag2.value(0)
-        mag3.value(1)
-        mag4.value(0)
-        time.sleep(0.005)
-        
-        mag1.value(0)
-        mag2.value(1)
-        mag3.value(0)
-        mag4.value(0)
-        time.sleep(0.005)
-        
-        mag1.value(1)
-        mag2.value(0)
-        mag3.value(0)
-        mag4.value(0)
-        time.sleep(0.005)
-        
-    for i in range (1, 500):
-        mag1.value(1)
-        mag2.value(0)
-        mag3.value(0)
-        mag4.value(0)
-        time.sleep(0.005)
+        for k in list2:
+            mag1.value(k[0])
+            mag2.value(k[1])
+            mag3.value(k[2])
+            mag4.value(k[3])
+            time.sleep(0.003)
